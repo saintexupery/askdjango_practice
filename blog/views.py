7fc50f7ad6c61a122d00059ebee83fec210fc209
Younggi Min <younggimin91@gmail.com>
2016-04-19 18:13:45 +0900

    init blog

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Post
+
+
+def post_list(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {
+        'post_list' : post_list,
+    })
+
+def post_detail(request):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {
+        'post' : post,
+    })
